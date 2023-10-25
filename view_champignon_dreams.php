1089333d92c4fa96282377507772e4693b2bbbc8
AlexandreGali <143389278+AlexandreGali@users.noreply.github.com>
2023-10-25 08:45:56 +0200

    Add files via upload

--- view_champignon_dreams.php
+++ view_champignon_dreams.php
@@ -15,13 +15,24 @@ function isPageActive($currentPage, $targetPage) {
 </div>
 
 <div class="titre-shop-champignon">
-  <h1>Shop Champignon Dreams</h1>
+  <?php
+      $sql = "SELECT * FROM produits WHERE id_produit = 2";
+      $stmt = $bdd->query($sql);
+      $produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      foreach ($produits as $produit) {
+          ?>
+        <h1>Shop <?= $produit['nom_produit']; ?></h1>
+        <?php
+                }
+            ?>
 </div>
 
 <div class="presentation-produit-champignon">
   <div class="presentation-texte">
     <div class="titre-produit">
-      <h3><i>Champignon</i> Dreams</h3>
+      <h3><?= $produit['nom_produit']; ?></h3>
+      
       <p>- Juicy and balanced -</p>
     </div>
     <div class="ingredient-produit">
@@ -61,24 +72,67 @@ function isPageActive($currentPage, $targetPage) {
       </div>
     
   </div>
-  <div class="presentation-bouteille" id="bottle-champignon3">
-    <img src="../../e-commerce/public/assets/img/champignon/produit-champignon-bottle.png" class="image-bg5" alt="BottleChampignon">
-    <img src="../../e-commerce/public/assets/img/champignon/produit-champignon-bottle-hover.png" class="image-overlay5" alt="BottleChampignonHover">
+
+  <?php
+
+// Récupérez les informations sur le premier produit souhaité
+$id_produit1 = 1;
+$sql_produit1 = "SELECT * FROM produits WHERE id_produit = :id_produit1";
+$stmt_produit1 = $bdd->prepare($sql_produit1);
+$stmt_produit1->bindParam(':id_produit1', $id_produit1, PDO::PARAM_INT);
+$stmt_produit1->execute();
+$produit1 = $stmt_produit1->fetch(PDO::FETCH_ASSOC);
+
+// Récupérez les images associées au premier produit par ordre d'affichage
+$sql_images1 = "SELECT * FROM images WHERE id_produit = :id_produit1 ORDER BY ordre_affichage";
+$stmt_images1 = $bdd->prepare($sql_images1);
+$stmt_images1->bindParam(':id_produit1', $id_produit1, PDO::PARAM_INT);
+$stmt_images1->execute();
+$images1 = $stmt_images1->fetchAll(PDO::FETCH_ASSOC);
+
+// Récupérez les informations sur le deuxième produit souhaité
+$id_produit2 = 2;
+$sql_produit2 = "SELECT * FROM produits WHERE id_produit = :id_produit2";
+$stmt_produit2 = $bdd->prepare($sql_produit2);
+$stmt_produit2->bindParam(':id_produit2', $id_produit2, PDO::PARAM_INT);
+$stmt_produit2->execute();
+$produit2 = $stmt_produit2->fetch(PDO::FETCH_ASSOC);
+
+// Récupérez les images associées au deuxième produit par ordre d'affichage
+$sql_images2 = "SELECT * FROM images WHERE id_produit = :id_produit2 ORDER BY ordre_affichage";
+$stmt_images2 = $bdd->prepare($sql_images2);
+$stmt_images2->bindParam(':id_produit2', $id_produit2, PDO::PARAM_INT);
+$stmt_images2->execute();
+$images2 = $stmt_images2->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="presentation-bouteille" id="bottle-champignon3">
+    <?php foreach ($images1 as $image) { 
+        if ($image['ordre_affichage'] == 1) { ?>
+            <img src="back-office/<?= $image['image'] ?>" alt="<?= $image['nom_img'] ?>">
+        <?php } 
+    } ?>
     <div class="texte-presentation-bottle">
-      <h4>Champignon dreams</h4>
-      <p>BOTTLE</p>
-      <a href="index.php?page=3&product=bottle">SHOP NOW</a>
+        <h4><?= $produit1['nom_produit']; ?></h4>
+        <p>BOTTLE</p>
+        <a href="index.php?page=3&product=bottle">SHOP NOW</a>
     </div>
-  </div>
-  <div class="presentation-can">
-    <img src="../../e-commerce/public/assets/img/champignon/produit-champignon-can.png" class="image-bg5" alt="CanChampignon">
-    <img src="../../e-commerce/public/assets/img/champignon/produit-champignon-can-hover.png" class="image-overlay5" alt="CanChampignonHover">
+</div>
+<div class="presentation-can">
+    <?php foreach ($images2 as $image) { 
+        if ($image['ordre_affichage'] == 2) { ?>
+            <img src="back-office/<?= $image['image'] ?>" alt="<?= $image['nom_img'] ?>">
+        <?php } 
+    } ?>
     <div class="texte-presentation-can">
-      <h4>Champignon dreams</h4>
-      <p>CAN</p>
-      <a href="index.php?page=3&product=can">SHOP NOW</a>
+        <h4><?= $produit2['nom_produit']; ?></h4>
+        <p>CAN</p>
+        <a href="index.php?page=3&product=can">SHOP NOW</a>
     </div>
-  </div>
+</div>
+
+
+
 </div>
 
 
