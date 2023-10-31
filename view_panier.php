c0cecfcea5ead4ae8a796fcafbb5eba786125a21
AlexandreGali <143389278+AlexandreGali@users.noreply.github.com>
2023-10-31 14:10:55 +0100

    Add files via upload

---
+++ view_panier.php
@@ -0,0 +1,52 @@
+<div class="container-page">
+
+    <div class="titre-page-panier">
+        <h1>My bag</h1>
+    </div>
+
+    <div class="entete-panier">
+        <div class="nom-produit-panier-entete">
+            <p>PRODUCT</p>
+        </div>
+        <div class="prix-produit-panier-entete">
+            <p>PRICE</p>
+        </div>
+        <div class="quantite-produit-panier-entete">
+            <p>QTY</p>
+        </div>
+        <div class="prix-total-panier-entete">
+            <p>TOTAL</p>
+        </div>
+    </div>
+
+    <div class="container-produit-panier">
+        <div class="photo-produit-panier">
+            <img src="public/assets/img/golden/produit-golden-can.png">
+        </div>
+        <div class="nom-produit-panier">
+            <h6>GOLDEN HOUR</h6>
+            <p>24 Cans</p>
+        </div>
+        <div class="prix-produit-panier">
+            <p>$135.00
+        </div>
+        <div class="quantite-produit-panier">
+            <p>1</p>
+        </div>
+        <div class="prix-total-panier">
+            <p>$135.00</p>
+        </div>
+    </div>
+
+    <div class="container-recap-prix-final">
+        <div class="recap-prix-final">
+            <h3>Subtotal</h3>
+            <p>$135.00</p>
+        </div>
+    </div>
+
+    <div class="checkout">
+        <button>CHECKOUT</button>
+    </div>
+
+</div>
